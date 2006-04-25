3a0744f3682d48bf1ae74f5ba36c282ad5ca14e6
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2006-04-25 19:11:21 +0000

    agora as ele verifica se um registro foi salvo ou nao

--- test/email_test.rb
+++ test/email_test.rb
@@ -12,41 +12,31 @@ class EmailsTest < Test::Unit::TestCase
     EmailData.validates_as_email :mail, :online => nil
     
     email_invalido = EmailData.create(:id => 1, :mail => "123aa@a.k")
-    email_invalido.save
 
-
-    assert ( email_invalido.errors.on(:mail) != nil ),
-             "Salvou email invalido."
+    assert ( not email_invalido.save ), "Salvou email invalido."
   end
 
   def test_email_valido_offline
     EmailData.validates_as_email :mail, :online => nil
     
     email_valido = EmailData.create(:id => 2, :mail => "gustavosbarreto@gmail.com")
-    email_valido.save
 
-
-    assert ( email_valido.errors.on(:mail) == nil ),
-             "Nao salvou um email valido que deveria ser salvo."
+    assert ( email_valido.save ), "Nao salvou um email valido que deveria ser salvo."
   end
 
   def test_email_invalido_online
     EmailData.validates_as_email :mail, :online => true
     
     email_invalido = EmailData.create(:id => 3, :mail => "ubirajararodrigues@debian.org")
-    email_invalido.save
 
-    assert ( email_invalido.errors.on(:mail) != nil ),
-             "Salvou email invalido."
+    assert ( not email_invalido.save ), "Salvou email invalido."
   end
 
   def test_email_valido_online
     EmailData.validates_as_email :mail, :online => true
    
     email_valido = EmailData.create(:id => 4, :mail => "gustavosbarreto@gmail.com")
-    email_valido.save
 
-    assert ( email_valido.errors.on(:mail) == nil ),
-             "Nao salvou um email valido que deveria ser salvo."
+    assert ( email_valido.save ), "Nao salvou um email valido que deveria ser salvo."
   end
 end
