ac0be6a2290ae9644030bc6a13676abe97983bed
Lucas Dutra Nunes <lucas.dutra.nunes@gmail.com>
2011-09-17 13:59:31 -0300

    Corrigidos alguns testes

--- lib/validates_as_email.rb
+++ lib/validates_as_email.rb
@@ -28,59 +28,68 @@ require 'rfc2822'
 include Socket::Constants
 
 module RFC2822
-  def RFC2822.check_addr_spec(email = nil)
-    # apenas valida o formato do email...
-    email =~ RFC2822::EmailAddress
-  end
+  class << self
+    def check_addr_spec(email = nil)
+      # apenas valida o formato do email...
+      email =~ RFC2822::EmailAddress
+    end
 
-  def RFC2822.check_addr_online(email = nil)
-    return true if email.blank?
+    def check_addr_online(email = nil)
+      return true if email.blank?
 
-    host = email.split("@")[1]
+      host = email.split("@")[1]
 
-    # Verifica se o host existe
-    Socket.gethostbyname(host)
+      # Verifica se o host existe
+      Socket.gethostbyname(host)
 
-    socket = Socket.new(AF_INET, SOCK_STREAM, 0)
+      socket = Socket.new(AF_INET, SOCK_STREAM, 0)
 
-    dns =  Resolv::DNS.new.getresources(host, Resolv::DNS::Resource::IN::MX)
-    mx_record = dns[0].exchange.to_s unless dns.empty?
+      dns =  Resolv::DNS.new.getresources(host, Resolv::DNS::Resource::IN::MX)
+      mx_record = dns[0].exchange.to_s unless dns.empty?
 
-    # Se o host nao tiver um MX Record usa o proprio host como SMTP
-    smtp_server = mx_record == nil ? host : mx_record
-    sockaddr = Socket.pack_sockaddr_in(25, smtp_server)
+      # Se o host nao tiver um MX Record usa o proprio host como SMTP
+      smtp_server = mx_record == nil ? host : mx_record
+      sockaddr = Socket.pack_sockaddr_in(25, smtp_server)
 
-    RAILS_DEFAULT_LOGGER.debug "#{Time.now} connecting to #{smtp_server} ------"
-    socket.connect(sockaddr)
+      debug "#{Time.now} connecting to #{smtp_server} ------"
+      socket.connect(sockaddr)
 
-    # Conectou? estamos prontos pra conversar?
-    if socket.recvfrom(255).to_s.chomp =~ /^220/
-      # Conversando...
-      RAILS_DEFAULT_LOGGER.debug "#{Time.now} HELO #{host}"
-      socket.write("HELO #{host}\r\n")
-      out = socket.recvfrom(255).to_s.chomp
-      RAILS_DEFAULT_LOGGER.debug "#{Time.now} #{out}"
+      # Conectou? estamos prontos pra conversar?
+      if socket.recvfrom(255).to_s.chomp =~ /^220/
+        # Conversando...
+        debug "#{Time.now} HELO #{host}"
+        socket.write("HELO #{host}\r\n")
+        out = socket.recvfrom(255).to_s.chomp
+        debug "#{Time.now} #{out}"
 
-      RAILS_DEFAULT_LOGGER.debug "#{Time.now} MAIL FROM: <#{email}>"
-      socket.write("MAIL FROM: <#{email}>\r\n")
-      out = socket.recvfrom(255).to_s.chomp
-      RAILS_DEFAULT_LOGGER.debug "#{Time.now} #{out}"
+        debug "#{Time.now} MAIL FROM: <#{email}>"
+        socket.write("MAIL FROM: <#{email}>\r\n")
+        out = socket.recvfrom(255).to_s.chomp
+        debug "#{Time.now} #{out}"
 
-      RAILS_DEFAULT_LOGGER.debug "#{Time.now} RCPT TO: <#{email}>"
-      socket.write("RCPT TO: <#{email}>\r\n")
-      out = socket.recvfrom(255).to_s.chomp
-      RAILS_DEFAULT_LOGGER.debug "#{Time.now} #{out}"
+        debug "#{Time.now} RCPT TO: <#{email}>"
+        socket.write("RCPT TO: <#{email}>\r\n")
+        out = socket.recvfrom(255).to_s.chomp
+        debug "#{Time.now} #{out}"
 
-      # Foi um prazer
-      RAILS_DEFAULT_LOGGER.debug "#{Time.now} QUIT"
-      socket.write("QUIT\r\n")
-      RAILS_DEFAULT_LOGGER.debug "#{Time.now} #{socket.recvfrom(255).to_s.chomp}"
-      socket.close
+        # Foi um prazer
+        debug "#{Time.now} QUIT"
+        socket.write("QUIT\r\n")
+        debug "#{Time.now} #{socket.recvfrom(255).to_s.chomp}"
+        socket.close
 
-      # Se a ultima coisa que o SMTP server enviou comecar com 250 o email existe, se não...
-      out =~ /^250/ ? true : nil
-    else
-      return nil
+        # Se a ultima coisa que o SMTP server enviou comecar com 250 o email existe, se não...
+        out =~ /^250/ ? true : nil
+      else
+        return nil
+      end
+    end
+
+    private
+    def debug(message)
+      if defined? RAILS_DEFAULT_LOGGER
+        RAILS_DEFAULT_LOGGER.debug message
+      end
     end
   end
 end
@@ -88,6 +97,8 @@ end
 module ActiveRecord
   module Validations
     module ClassMethods
+      class InvalidEmail < Exception; end
+
       def validates_as_email(*attr_names)
         configuration = {
           :message => :invalid,
@@ -100,19 +111,19 @@ module ActiveRecord
         validates_each(attr_names, configuration) do |record, attr_name, value|
           unless value.blank?
             begin
-              value.split(',').each do |email|
-                email.strip! if configuration[:multiple]
-                # levanta excessao se estiver na blacklist
-                raise unless configuration[:blacklist].select {|regex| email =~ regex}.empty?
-                # levanta excessao padrao se nao validar o endereco
-                raise unless RFC2822::check_addr_spec(email)
-                # levanta excessao padrao se for pra validar online e nao passar no teste
-                raise if configuration[:online] and not RFC2822::check_addr_online(email)
+              emails = configuration[:multiple] ? value.split(',').collect{|email| email.strip!} : [value]
+              emails.each do |email|
+                # levanta exceção se estiver na blacklist
+                raise InvalidEmail unless configuration[:blacklist].select {|regex| email =~ regex}.empty?
+                # levanta exceção padrao se nao validar o endereco
+                raise InvalidEmail unless RFC2822::check_addr_spec(email)
+                # levanta exceção padrao se for pra validar online e nao passar no teste
+                raise InvalidEmail if configuration[:online] and not RFC2822::check_addr_online(email)
               end
             rescue Errno::ETIMEDOUT
               # pode ocorrer no check_addr_online
               record.errors.add(attr_name, configuration[:timeout])
-            rescue StandardError, SocketError, Errno::ECONNREFUSED
+            rescue InvalidEmail, SocketError, Errno::ECONNREFUSED
               record.errors.add(attr_name, configuration[:message])
             end
           end
